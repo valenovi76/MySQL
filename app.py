c2e95362b7668ff4c75300522a3958c04bb3456a
valenovi76 <valentina.novara@bt.com>
2020-03-19 21:58:22 +0000

    submit button added and new css file

--- app.py
+++ app.py
@@ -12,14 +12,26 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGODB_NAME"] = os.environ.get("MONGODB_NAME" ,'mongodb://localhost') 
 
 mongo = PyMongo(app)
+
+
 @app.route('/')
-#@app.route('/requests')
-#def get_requests():
-   # return render_template("requests.html", requests=mongo.db.c_requests.find())
+
+
+@app.route('/get_requests')
+def get_requests():
+    return render_template("requests.html", requests=mongo.db.c_requests.find())
+
 
 @app.route('/new_requests')
-def get_new_requests():
+def add_new_requests():
     return render_template("new_requests.html", foods=mongo.db.c_food.find())
+
+
+@app.route('/insert_requests', methods=['POST'])
+def insert_requests():
+    requests = mongo.db.c_requests
+    requests.insert_one(request.form.to_dict())
+    return redirect(url_for('get_requests')) 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
