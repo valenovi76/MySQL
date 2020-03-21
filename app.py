65f136c7f58671554fd6c34910d35f4e39c5aa89
valenovi76 <valentina.novara@bt.com>
2020-03-21 15:07:42 +0000

    multiple checkbox fixed

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ mongo = PyMongo(app)
 @app.route('/')
 
 
-#@app.route('/get_requests')
-#def get_requests():
-#    return render_template("requests.html", requests=mongo.db.c_requests.find())
+@app.route('/get_requests')
+def get_requests():
+   return render_template("requests.html", requests=mongo.db.c_requests.find())
 
 
 @app.route('/new_requests')
@@ -33,12 +33,12 @@ def insert_requests():
     requests.insert_one(request.form.to_dict())  #many
     return redirect(url_for('get_requests')) 
 
-#@app.route('/edit_requests/<request_id>')
-#def edit_requests(request_id):
-#    the_request =  mongo.db.c_requests.find_one({"_id": ObjectId(requests_id)})
- #   all_qty =  mongo.db.c_qty.find()
- #   return render_template('edit_requests.html', request=the_request,
-  #                         categories=all_qty)
+@app.route('/edit_requests/<request_id>')
+def edit_requests(request_id):
+the_request =  mongo.db.c_requests.find_one({"_id": ObjectId(requests_id)})
+ all_qty =  mongo.db.c_qty.find()
+  return render_template('edit_requests.html', request=the_request,
+                         categories=all_qty)
 
 
 if __name__ == '__main__':
