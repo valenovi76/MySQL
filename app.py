65d792dfa224c19a0f620a61a00d6bf41fc7ec30
valenovi76 <valentina.novara@bt.com>
2020-03-22 12:10:03 +0000

    python function to store hostname set up

--- app.py
+++ app.py
@@ -5,31 +5,7 @@ if path.exists("env.py"):
 from flask import Flask, request, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
-
-# Python3 code to display hostname and 
-# IP address 
-  
-# Importing socket library 
-import socket 
-  
-# Function to display hostname and 
-# IP address 
-def get_Host_name_IP(): 
-    try: 
-        host_name = socket.gethostname() 
-        host_ip = socket.gethostbyname(host_name) 
-        print("Hostname :  ",host_name) 
-        print("IP : ",host_ip) 
-    except: 
-        print("Unable to get Hostname and IP") 
-  
-# Driver code 
-get_Host_name_IP() #Function call 
-  
-#This code is conributed by "Sharad_Bhardwaj". 
-
-
+import socket
 
 
 app = Flask(__name__)
@@ -46,19 +22,35 @@ mongo = PyMongo(app)
 def get_requests():
     return render_template("requests.html", requests=mongo.db.c_requests.find())
 
-
+# Function to display foodtable from db
 @app.route('/new_requests')
 def add_new_requests():
     return render_template("new_requests.html", foods=mongo.db.c_food.find())
+# Function to display hostname and 
+# IP address 
+
+
+def get_Host_name_IP(): 
+    host_name = socket.gethostname() 
+#    host_ip = socket.gethostbyname(host_name) 
+    return host_name
+#This code is conributed by "Sharad_Bhardwaj". 
+
+
+@app.route('/insert_requests', methods=['POST'])
+def insert_requests():
+    requests = mongo.db.c_requests
+    requests.insert_one(request.form.to_dict())
+    return redirect(url_for('get_requests'))
 
 
 @app.route('/my_requests')
-def get_requests():
+def my_requests():
     return render_template("my_requests.html", requests=mongo.db.c_requests.find(host_name))   
 
 
 @app.route('/udate_requests')
-def get_requests():
+def update_requests():
     return render_template("update_requests.html", requests=mongo.db.c_requests.find(host_name))
 
 if __name__ == '__main__':
