715f8c8b5039e183583a12e6ae82336f13a0f9a9
valenovi76 <valentina.novara@bt.com>
2020-03-25 21:00:42 +0000

    login page completed

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-import socket
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -11,18 +10,26 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["MONGODB_NAME"] = os.environ.get("MONGODB_NAME" ,'mongodb://localhost') 
-app.secret_key = "randomstring123"
-messages = []
-
+app.config["MONGODB_NAME"] = os.environ.get("MONGODB_NAME",
+                                            'mongodb://localhost')
 
 mongo = PyMongo(app)
 
+
+@app.route('/')
+
+
 @app.route('/get_requests')
 def get_requests():
-    return render_template("requests.html", requests=mongo.db.c_requests.find())
+    return render_template("requests.html",
+                           requests=mongo.db.c_requests.find())
 
-# Function to display foodtable from db
+
+@app.route('/index.html')
+def index():
+    return render_template("index.html", memebers=mongo.db.c_members.find())
+
+
 @app.route('/new_requests')
 def add_new_requests():
     return render_template("new_requests.html", foods=mongo.db.c_food.find())
@@ -40,20 +47,6 @@ def insert_login():
     logins = mongo.db.c_user
     logins.insert_one(request.form.to_dict())
     return redirect(url_for('add_new_requests'))
-
-
-def get_hostname():
-    hostname = socket.gethostname()
-    return hostname
-
-#@app.route('/my_requests')
-#def my_requests():
-#return render_template("my_requests.html", requests=mongo.db.c_requests.find(hostname))   
-
-
-#@app.route('/udate_requests')
-#def update_requests():
-#return render_template("update_requests.html", requests=mongo.db.c_requests.find(host_name))
 
 
 if __name__ == '__main__':
--- test.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/api/v1/users/", methods=['GET', 'POST', 'PUT'])
-def users():
