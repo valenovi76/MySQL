573b75620cdc7be5fa57092edffc4bada2c80e39
valenovi76 <valentina.novara@bt.com>
2020-03-28 22:57:56 +0000

    update ruotine to be fixed

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ mongo = PyMongo(app)
 @app.route('/new_requests')
 def index():
     return render_template("index.html", foods=mongo.db.c_food.find(),
-                           members=mongo.db.c_members.find(), shops=mongo.db.c_shops.find())
+                           members=mongo.db.c_members.find(),
+                           shops=mongo.db.c_shops.find(),
+                           status=mongo.db.c_status.find())
 
 
 @app.route('/member_reg')
@@ -36,7 +38,7 @@ def requests():
 
 @app.route('/insert_requests', methods=['POST'])
 def insert_requests():
-    requests = mongo.db.c_requests
+    request = mongo.db.c_requests
     requests.insert_one(request.form.to_dict())
     return redirect(url_for('requests'))
 
@@ -46,6 +48,31 @@ def insert_login():
     logins = mongo.db.c_user
     logins.insert_one(request.form.to_dict())
     return redirect(url_for('add_new_requests'))
+
+
+@app.route('/edit_request/<request_id>')
+def edit_request(request_id):
+    the_request = mongo.db.c_requests.find_one({"_id": ObjectId(request_id)})
+    all_members = mongo.db.c_members.find()
+    all_shops = mongo.db.c_shops.find()
+    all_status = mongo.db.c_status.find()
+    return render_template("edit_request.html", request=the_request,
+                           members=all_members, shops=all_shops,
+                           status=all_status)
+
+
+@app.route('/update_request/<request_id>', methods=["POST"])
+def update_request(request_id):
+    request = mongo.db.c_requests
+    request.update({"_id": ObjectId(request_id)},
+                   {'requested_by': request.form.get('requested_by'),
+                    'phone': request.form.get('phone'),
+                    'shop': request.form.get('shop'),
+                    'date': request.form.get('date'),
+                    'food': request.form.get('food'),
+                    'status': request.form.get('status')
+                    })
+    return redirect(url_for('requests.html'))
 
 
 if __name__ == '__main__':
